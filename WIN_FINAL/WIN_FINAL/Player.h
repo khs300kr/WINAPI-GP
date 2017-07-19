a6755b0cd3978f85e29586777126121e1824e085
KIM HUISEUNG <khs300kr@naver.com>
2017-07-19 17:28:15 +0900

    win api gp commit

---
+++ WIN_FINAL/WIN_FINAL/Player.h
@@ -0,0 +1,18 @@
+#pragma once
+class CPlayer
+{
+private:
+	int m_iX, m_iY; 
+	int m_iTurn;    
+	int m_iFrameX;  
+	int m_iFrameY;  
+	int m_iScore;   
+
+public:
+	CPlayer();
+	~CPlayer();
+
+public:
+
+};
+
